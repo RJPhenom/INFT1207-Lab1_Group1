beecbc744ca44994e06e80d785660bd2edbb4857
RJ <rj@macklem.ca>
2024-01-25 18:30:28 -0500

    Initial Commi
    Initial Commit for Lab 1 Group 1, since we were unable to meet in person I set up my proj via git to collaborate.

---
+++ Lab1_robert_afoma.py
@@ -0,0 +1,9 @@
+###############################################################################
+#   Class:          INFT1207
+#   Authors:        Robert Macklem and Afoma Egwuatu
+#   Date:           January 23 2024
+#   File:           Lab1_robert_afoma.py
+#   Description:    A strong password generator that takes length and number
+#                   of different character inputs from the user to generate
+#                   a strong password.
+###############################################################################
