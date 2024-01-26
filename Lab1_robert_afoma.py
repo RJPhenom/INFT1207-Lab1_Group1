b2ee1f19c4300ab171ed0b8258b7e11907624b5e
RJ <rj@macklem.ca>
2024-01-25 19:31:51 -0500

    Update Lab1_robert_afoma.py
    Updated the main .py file with extended logic framework to build upon. Added a function to build a password string, added input loop to get password length, added final return of strong password and output statement.

--- Lab1_robert_afoma.py
+++ Lab1_robert_afoma.py
@@ -12,6 +12,7 @@
 # CONSTS
 # Const minimum password length
 MIN_PASSWORD_LENGTH = 8
+MAX_PASSWORD_LENGTH = 24
 
 # Const minimum(s) for three key char types:
 # (alphabetical, numeric, and special)
@@ -31,8 +32,12 @@ CHAR_TYPES = {
 # Variables that track the total length and how many of each
 # char we need (note, alpha = len-(special+numeric)).
 password_len = 0
-special_chars = 0
-numeric_chars = 0
+alphabet_chars_num = 0
+numeric_chars_num = 0
+special_chars_num = 0
+
+# Var to store the actual password
+password = ""
 
 
 # FUNCTIONS
@@ -50,8 +55,52 @@ def get_chars_of_type(num: int, char_type: str):
     return chars_found
 
 
+# build_password_with_chars is the main function called to build the final password string
+# that we will return to the user. It calls get_chars_of_type on our three main char types
+# to build an unsorted string, then randomizes the position of each char in a string, then
+# returns the randomized string as a strong password.
+def build_password_with_chars(alphabet_num: int, numeric_num: int, special_num: int):
+    password = ""
+    password += (get_chars_of_type(alphabet_num, "alphabet"),
+                 get_chars_of_type(numeric_num, "numeric"),
+                 get_chars_of_type(special_num, "special"))
+
+    # TODO: randomize the chars position in the password
+    return password
+
+
 # PROGRAM
 # Startup message
 print("\nRandom Password Generator\n")
 print(get_chars_of_type(1, "alphabet")) # testing only, to be removed
+
+# Get the desired password length
+while password_len == 0:
+    valid_length = False
+    try:
+        proposed_length = int(input("Please input your desired password length: "))
+        if proposed_length < MIN_PASSWORD_LENGTH or proposed_length > MAX_PASSWORD_LENGTH:
+            print(f"\nPassword must be within the length range of {MIN_PASSWORD_LENGTH} to {MAX_PASSWORD_LENGTH}.")
+            print("Please input a new desired password length.\n")
+        else:
+            password_len = proposed_length
+    except:
+        print("\n***INPUT ERROR***\nPlease use numeric characters only.\n")
+
+
+# Get the desired numeric characters
+# TODO: Add similar while loop to above for special chars
+
+
+# Get the desired numeric characters
+# TODO: Add similar while loop to above for special chars
+
+
+# Get the desired numeric characters
+alphabet_chars_num = password_len - (numeric_chars_num + special_chars_num)
+
+
+# Build the password
+password = build_password_with_chars(alphabet_chars_num, numeric_chars_num, special_chars_num)
+print(f"Your strong password is: {password}")
 
